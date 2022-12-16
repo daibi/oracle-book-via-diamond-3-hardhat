54131770579499c021cf07808dd3b243889a5d5f
Ruofan <dbfornewsletter@outlook.com>
2022-12-16 08:47:21 +0000

    [test]: add faithful facet test [feature]: fortune cookie facet init

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,23 +38,7 @@ module.exports = {
     hardhat: {
       initialBaseFeePerGas: 0,
       blockGasLimit: 18800000,
-    },
-    local_ganache: {
-      url: `HTTP://127.0.0.1:7545`,
-      accounts: [`0x260d12c5b92e837709d94972ebc42b89c74fae72affbdd2090be0caf51eac55b`],
-    },
-    polygon_mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`0x` + process.env.PRIVATE_KEY]
-    },
-    polygon_mumbai_infura: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x` + process.env.PRIVATE_KEY]
-    },
-    polygon_mumbai_matic_vigil: {
-      url: `https://rpc-mainnet.maticvigil.com/v1/${process.env.MATIC_VIGIL_API_KEY}`,
-      accounts: [`0x` + process.env.PRIVATE_KEY]
-    },
+    }
   },
   namedAccounts: {
     deployer: 0
